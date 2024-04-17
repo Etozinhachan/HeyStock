eccd404aafff8811d65e2d0058959dd79ed475ed
Etozinhachan <etochandev@gmail.com>
2024-04-17 07:20:44 +0100

    last attempt

--- wwwroot/js/menu_vendas.js
+++ wwwroot/js/menu_vendas.js
@@ -1,4 +1,8 @@
 const open_dialog_btn = document.querySelector(".open_dialog")
 const dialog = document.querySelector("dialog")
 
-open_dialog_btn.addEventListener("click", () => { dialog.showModal() })
+function open_dialog_event_handler(){
+    dialog.showModal()
+}
+
+open_dialog_btn.addEventListener("click", () => { open_dialog_event_handler() })
